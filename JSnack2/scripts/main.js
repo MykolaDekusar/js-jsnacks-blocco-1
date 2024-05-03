09a4dc05b010a97de21c3794df48e2c86de853ac
MykolaDekusar <ndekusar97@gmail.com>
2024-05-03 11:44:02 +0200

    [END]End

--- JSnack2/scripts/main.js
+++ JSnack2/scripts/main.js
@@ -5,7 +5,16 @@ const parola2 = (prompt("Inserisci la seconda parola")).length;
 
 if (parola1 > parola2) {
     alert("La prima parola è più lunga con " + parola1 + " caratteri contro i " + parola2 + " caratteri");
+    console.log(parola2);
+    console.log(parola1);
 } else if (parola2 > parola1) {
     alert("La seconda parola è più lunga" + parola2 + " caratteri contro i " + parola1 + " caratteri");
-} else (alert("Le parole sono di lunghezza uguale con " + parola1 + " caratteri"));
+    console.log(parola1);
+    console.log(parola2);
+} else {
+    alert("Le parole sono di lunghezza uguale con " + parola1 + " caratteri");
+    console.log(parola1);
+    console.log(parola2);
+}
+
 
