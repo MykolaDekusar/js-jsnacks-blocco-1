a470ea0d38834f8bac430ada915ba8028012d810
MykolaDekusar <ndekusar97@gmail.com>
2024-05-03 13:21:06 +0200

    [FIX]Fixed control and added comments

--- JSnack 3/scripts/main.js
+++ JSnack 3/scripts/main.js
@@ -4,13 +4,17 @@ let somma;
 let totale = 0;
 
 for (let i = 0; i < nInput; i++) {
+    //chiedo all'utente nInput numeri
     somma = Number(prompt("Inserisci un numero"));
-    if (somma > 0) {
+    //controllo se il numero inserito è un numero
+    if (!(isNaN(somma))) {
         console.log(somma);
         totale += somma;
         console.log(totale);
+        //se non lo è allora richiedo
     } else {
         alert("Inserisci un numero valido");
+        // i= i-1
         i--;
     }
 
