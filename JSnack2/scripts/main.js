117358e39caddaabbc12e417406c03edfaaa367a
MykolaDekusar <ndekusar97@gmail.com>
2024-05-03 11:47:13 +0200

    [END]END

--- JSnack2/scripts/main.js
+++ JSnack2/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const parola1 = (prompt("Inserisci la prima parola")).length;
-const parola2 = (prompt("Inserisci la seconda parola")).length;
+const parola1 = prompt("Inserisci la prima parola");
+const parola2 = prompt("Inserisci la seconda parola");
+const lParola1 = parola1.length;
+const lParola2 = parola2.length;
 
-if (parola1 > parola2) {
+if (lParola1 > lParola2) {
     alert("La prima parola è più lunga con " + parola1 + " caratteri contro i " + parola2 + " caratteri");
     console.log(parola2);
     console.log(parola1);
-} else if (parola2 > parola1) {
-    alert("La seconda parola è più lunga" + parola2 + " caratteri contro i " + parola1 + " caratteri");
+} else if (lParola2 > lParola1) {
+    alert("La seconda parola è più lunga " + parola2 + " caratteri contro i " + parola1 + " caratteri");
     console.log(parola1);
     console.log(parola2);
 } else {
