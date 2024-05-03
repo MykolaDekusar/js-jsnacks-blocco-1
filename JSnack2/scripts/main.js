181e9b70eb7940ff59243aaaeb71b10b926e52d1
MykolaDekusar <ndekusar97@gmail.com>
2024-05-03 11:42:31 +0200

    [END]End of exercise

--- JSnack2/scripts/main.js
+++ JSnack2/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const parola1 = (prompt("Inserisci la prima parola")).length;
+const parola2 = (prompt("Inserisci la seconda parola")).length;
+
+if (parola1 > parola2) {
+    alert("La prima parola è più lunga con " + parola1 + " caratteri contro i " + parola2 + " caratteri");
+} else if (parola2 > parola1) {
+    alert("La seconda parola è più lunga" + parola2 + " caratteri contro i " + parola1 + " caratteri");
+} else (alert("Le parole sono di lunghezza uguale con " + parola1 + " caratteri"));
+
